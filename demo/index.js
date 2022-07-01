76c7ea19734bc9e07280fd4dcc92b49583d19174
Taisuke Fukuno <fukuno@jig.jp>
2022-07-02 08:35:54 +0900

    to modern JS

--- demo/index.js
+++ demo/index.js
@@ -1,98 +1,112 @@
 import WindGL from "./WindGL.js";
 import { GUI } from "https://cdn.jsdelivr.net/npm/lil-gui@0.16.1/dist/lil-gui.esm.min.js";
 
-// using var to work around a WebKit bug
-var canvas = document.getElementById('canvas'); // eslint-disable-line
+// util
+export const fetchJSON = async (url) => (await fetch(url)).json();
 
-const pxRatio = Math.max(Math.floor(window.devicePixelRatio) || 1, 2);
+export const addCSS = (css) => {
+  const getStyle = () => {
+    if (!document.styleSheets.length) {
+      const style = document.createElement("style");
+      document.head.appendChild(style);
+    }
+    return document.styleSheets[0];
+  };
+  const style = getStyle();
+  style.insertRule(css);
+};
+
+// coast
+const drawCoast = async () => {
+  const urlCoastline = "https://d2ad6b4ur7yvpq.cloudfront.net/naturalearth-3.3.0/ne_110m_coastline.geojson";
+  const data = await fetchJSON(urlCoastline);
+  const canvas = document.getElementById("coastline");
+  canvas.width = canvas.clientWidth * pxRatio;
+  canvas.height = canvas.clientHeight * pxRatio;
+
+  const ctx = canvas.getContext("2d");
+  ctx.lineWidth = pxRatio;
+  ctx.lineJoin = ctx.lineCap = "round";
+  ctx.strokeStyle = "white";
+  ctx.beginPath();
+
+  for (let i = 0; i < data.features.length; i++) {
+    const line = data.features[i].geometry.coordinates;
+    for (let j = 0; j < line.length; j++) {
+      ctx[j ? "lineTo" : "moveTo"](
+        (line[j][0] + 180) * canvas.width / 360,
+        (-line[j][1] + 90) * canvas.height / 180
+      );
+    }
+  }
+  ctx.stroke();
+};
+drawCoast();
+
+// wind
+const canvas = document.getElementById("canvas");
+
+const pxRatio = window.devicePixelRatio || 1;
 canvas.width = canvas.clientWidth;
 canvas.height = canvas.clientHeight;
 
-const gl = canvas.getContext('webgl', {antialiasing: false});
+const gl = canvas.getContext("webgl", { antialiasing: false });
 
 const wind = window.wind = new WindGL(gl);
 wind.numParticles = 65536;
 
-function frame() {
-    if (wind.windData) {
-        wind.draw();
-    }
-    requestAnimationFrame(frame);
-}
+const frame = () => {
+  if (wind.windData) {
+    wind.draw();
+  }
+  requestAnimationFrame(frame);
+};
 frame();
 
 const gui = new GUI();
-gui.add(wind, 'numParticles', 1024, 589824);
-gui.add(wind, 'fadeOpacity', 0.96, 0.999).step(0.001).updateDisplay();
-gui.add(wind, 'speedFactor', 0.05, 1.0);
-gui.add(wind, 'dropRate', 0, 0.1);
-gui.add(wind, 'dropRateBump', 0, 0.2);
+addCSS(".lil-gui.autoPlace { left: 15px; }");
 
-const windFiles = {
-    0: '2021062700',
-   24: '2021062800',
-   48: '2021062900',
-   72: '2021063000',
-   96: '2021070100'
-};
+gui.add(wind, "numParticles", 1024, 589824);
+gui.add(wind, "fadeOpacity", 0.96, 0.999).step(0.001).updateDisplay();
+gui.add(wind, "speedFactor", 0.05, 1.0);
+gui.add(wind, "dropRate", 0, 0.1);
+gui.add(wind, "dropRateBump", 0, 0.2);
+
+const url = "https://github.com/taisukef/webgl-wind";
 
 const meta = {
-    '2021-06-27+h': 0,
-    'retina resolution': true,
-    'github.com/mapbox/webgl-wind': function () {
-        window.location = 'https://github.com/mapbox/webgl-wind';
-    }
+  "2021-06-27+h": 0,
+  "retina resolution": true,
 };
-gui.add(meta, '2021-06-27+h', 0, 96, 24).onFinishChange(updateWind);
+meta[url] = () => window.location = url;
+
+const windFiles = {
+   0: "2021062700",
+  24: "2021062800",
+  48: "2021062900",
+  72: "2021063000",
+  96: "2021070100",
+};
+const updateWind = async (name) => {
+  const windData = await fetchJSON("wind/" + windFiles[name] + ".json");
+  const windImage = new Image();
+  windData.image = windImage;
+  windImage.src = "wind/" + windFiles[name] + ".png";
+  windImage.onload = () => wind.setWind(windData);
+};
+gui.add(meta, "2021-06-27+h", 0, 96, 24).onFinishChange(updateWind);
+
+const updateRetina = () => {
+  const ratio = meta["retina resolution"] ? pxRatio : 1;
+  canvas.width = canvas.clientWidth * ratio;
+  canvas.height = canvas.clientHeight * ratio;
+  wind.resize();
+};
 if (pxRatio !== 1) {
-    gui.add(meta, 'retina resolution').onFinishChange(updateRetina);
+  gui.add(meta, "retina resolution").onFinishChange(updateRetina);
 }
-gui.add(meta, 'github.com/mapbox/webgl-wind');
+gui.add(meta, url);
 gui.close();
 
 updateWind(0);
 updateRetina();
-
-function updateRetina() {
-    const ratio = meta['retina resolution'] ? pxRatio : 1;
-    canvas.width = canvas.clientWidth * ratio;
-    canvas.height = canvas.clientHeight * ratio;
-    wind.resize();
-}
-
-getJSON('https://d2ad6b4ur7yvpq.cloudfront.net/naturalearth-3.3.0/ne_110m_coastline.geojson', function (data) {
-    const canvas = document.getElementById('coastline');
-    canvas.width = canvas.clientWidth * pxRatio;
-    canvas.height = canvas.clientHeight * pxRatio;
-
-    const ctx = canvas.getContext('2d');
-    ctx.lineWidth = pxRatio;
-    ctx.lineJoin = ctx.lineCap = 'round';
-    ctx.strokeStyle = 'white';
-    ctx.beginPath();
-
-    for (let i = 0; i < data.features.length; i++) {
-        const line = data.features[i].geometry.coordinates;
-        for (let j = 0; j < line.length; j++) {
-            ctx[j ? 'lineTo' : 'moveTo'](
-                (line[j][0] + 180) * canvas.width / 360,
-                (-line[j][1] + 90) * canvas.height / 180);
-        }
-    }
-    ctx.stroke();
-});
-
-function updateWind(name) {
-    getJSON('wind/' + windFiles[name] + '.json', function (windData) {
-        const windImage = new Image();
-        windData.image = windImage;
-        windImage.src = 'wind/' + windFiles[name] + '.png';
-        windImage.onload = function () {
-            wind.setWind(windData);
-        };
-    });
-}
-
-function getJSON(url, callback) {
-    fetch(url).then(res => res.json().then(callback));
-};
