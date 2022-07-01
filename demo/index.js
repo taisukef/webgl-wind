8c85cc60e2f62cea917728021a5ba4a5083895b0
Taisuke Fukuno <fukuno@jig.jp>
2022-07-02 07:51:45 +0900

    use fetch instead of XMLHttpRequest

--- demo/index.js
+++ demo/index.js
@@ -94,15 +94,5 @@ function updateWind(name) {
 }
 
 function getJSON(url, callback) {
-    const xhr = new XMLHttpRequest();
-    xhr.responseType = 'json';
-    xhr.open('get', url, true);
-    xhr.onload = function () {
-        if (xhr.status >= 200 && xhr.status < 300) {
-            callback(xhr.response);
-        } else {
-            throw new Error(xhr.statusText);
-        }
-    };
-    xhr.send();
-}
+    fetch(url).then(res => res.json().then(callback));
+};
